caf182f335b08e9a19978a3c6e62a46abc2c852a
guomaoqiu@sina.com <guomaoqiu@sina.com>
2018-09-13 10:56:00 +0800

    🔞Initial commit

--- flask_app_code/app/main/forms.py
+++
@@ -1,8 +0,0 @@
-from flask_wtf import Form
-from wtforms import StringField, SubmitField
-from wtforms.validators import Required
-
-
-class NameForm(Form):
-    name = StringField('What is your name?', validators=[Required()])
-    submit = SubmitField('Submit')
